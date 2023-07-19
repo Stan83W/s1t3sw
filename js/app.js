ba339a699f65d2753af3381acfd48a5411901993
Stanislas Wiart <stanislas.wiart@gmail.com>
2023-07-19 17:30:01 +0200

    turn and face the change

---
+++ js/app.js
@@ -0,0 +1,8 @@
+    // Collapse
+
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.collapsible');
+      var instances = M.Collapsible.init(elems, {
+        accordion: true
+      });
+    });
